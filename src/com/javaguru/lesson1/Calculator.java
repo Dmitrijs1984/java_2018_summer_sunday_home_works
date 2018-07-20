73c9512102fb0801a2a72afb80b1a1ae31295e04
Dmitrijs1984 <41103709+Dmitrijs1984@users.noreply.github.com>
2018-07-20 10:39:11 +0300

    Delete Calculator.java

--- src/com/javaguru/lesson1/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.lesson1;
-
-class Calculator {
-
-    public int sum(int firstNumber, int secondNumber) {
-        int sumResult = firstNumber + secondNumber;
-        return sumResult;
-    }
-}
