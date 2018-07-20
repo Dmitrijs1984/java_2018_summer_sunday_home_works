166166b8ccc7b7b6fd47dd268cf549337f96ac2f
Dmitrijs1984 <41103709+Dmitrijs1984@users.noreply.github.com>
2018-07-20 10:38:29 +0300

    Delete SquareCalculator.java

--- src/com/javaguru/lesson2/SquareCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.javaguru.lesson2;
-
-class SquareCalculator {
-
-    public int calculateSquare(int number) {
-        int squareResult = number * number;
-        return squareResult;
-    }
-}
