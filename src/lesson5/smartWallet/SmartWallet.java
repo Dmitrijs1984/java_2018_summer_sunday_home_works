b46cc3107a2c2bda19644297ec174c377be0bb65
Dmitrijs1984 <dmitrijs.sirica@gmail.com>
2018-07-27 22:35:20 +0300

    SmartWallet

--- src/lesson5/smartWallet/SmartWallet.java
+++ src/lesson5/smartWallet/SmartWallet.java
@@ -2,4 +2,41 @@ package lesson5.smartWallet;
 
 public class SmartWallet {
 
-//asdf}
+    int correctPin = 123;
+    int amount = 0;
+    boolean userAccept = false;
+
+    void createSmartWallet(int correctPin, int amount) {
+        this.correctPin = correctPin;
+        this.amount = amount;
+    }
+
+    boolean addMoneyWallet(int pin, int addMoney) {
+        if (pin == correctPin) {
+            amount = amount + addMoney;
+            userAccept = true;
+        } else {
+            userAccept = false;
+        }
+        return userAccept;
+    }
+
+    boolean redraw(int pin, int getMoney) {
+        if ((pin == correctPin) && (amount - getMoney >= 0)) {
+            amount = amount - getMoney;
+            userAccept = true;
+        } else {
+            userAccept = false;
+        }
+        return userAccept;
+    }
+
+    int balance(int pin) {
+        if (pin == correctPin) {
+            userAccept=true;
+            return amount;
+        } else userAccept = false;
+
+        return 0;
+    }
+}
